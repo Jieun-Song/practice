82b2d1165042d29b6ee90fdb1af15cc0ff85aedf
jieun song <0scarl1216@mju.ac.kr>
2021-08-10 16:47:50 +0900

    ~210

---
+++ 파이썬 300 예제 풀기/201-210.py
@@ -0,0 +1,72 @@
+# 201, 202
+# def print_coin():
+#     print("비트코인")
+
+
+# print_coin()
+
+# 203
+
+# def print_coin():
+#     print("비트코인")
+
+
+# for i in range(100):
+#     print_coin()
+
+# 204
+
+# def print_coin():
+#     for i in range(100):
+#         print("비트코인")
+
+#     print_coin()
+
+# 205
+
+# 먼저 선언이 안되어서
+
+# 206
+
+# def message() :
+#     print("A")
+#     print("B")
+
+# message()
+# print("C")
+# message()
+
+# #A
+# #B
+# #C
+# #A
+# #B
+
+# 207
+
+# A
+# C
+# B
+
+# 208
+
+# A
+# C
+# B
+# E
+# D
+
+# 209
+
+# B
+# A
+
+# 210
+
+# B
+# C
+# B
+# c
+# B
+# C
+# A
