29c577be0d4c86ceb30f8e07368f11227204c6dc
jieun song <0scarl1216@mju.ac.kr>
2021-10-25 11:45:51 +0900

    week1

---
+++ crawling_Baekjoon/입출력과 사칙연산/01_10__.py
@@ -0,0 +1,10 @@
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+print((a+b) % c)
+print(((a % c)+(b % c)) % c)
+print((a*b) % c)
+print(((a*c)*(b % c)) % c)
+
+# 예제대로 답도 나오는데 왜 실패라고 뜰까, , ?
---
+++ crawling_Baekjoon/입출력과 사칙연산/01_11.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+a = int(a)
+b = int(b)
+b_1 = b//100
+b_2 = (b % 100)//10
+b_3 = b % 10
+print(a*b_1)
+print(a*b_2)
+print(a*b_3)
+print(a*b)
